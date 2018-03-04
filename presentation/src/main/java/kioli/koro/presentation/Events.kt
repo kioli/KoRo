ab8da08470d84cf73a3227f4cc23c3b7c17d5511
kioli <kioli85@gmail.com>
2018-03-04 11:29:57 +0100

    Add analytics

---
+++ presentation/src/main/java/kioli/koro/presentation/Events.kt
@@ -0,0 +1,15 @@
+package kioli.koro.presentation
+
+val eventResult = "Result"
+
+enum class Events(val id: String) {
+    LoadQuoteStart("load quote start"),
+    LoadQuoteResultOk("load quote result ok"),
+    LoadQuoteResultFail("load quote result fail"),
+    SaveQuoteStart("save quote start"),
+    SaveQuoteResultOk("save quote result ok"),
+    SaveQuoteResultFail("save quote result fail"),
+    ClearQuoteStart("clear quote start"),
+    ClearQuoteResultOk("clear quote result ok"),
+    ClearQuoteResultFail("clear quote result fail")
+}
--- presentation/src/main/java/kioli/koro/presentation/viewmodel/IntroViewModel.kt
+++ presentation/src/main/java/kioli/koro/presentation/viewmodel/IntroViewModel.kt
@@ -3,14 +3,17 @@ package kioli.koro.presentation.viewmodel
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.microsoft.appcenter.analytics.Analytics
 import io.reactivex.observers.DisposableCompletableObserver
 import io.reactivex.subscribers.DisposableSubscriber
 import kioli.koro.domain.interactor.ClearQuoteUseCase
 import kioli.koro.domain.interactor.LoadQuoteUseCase
 import kioli.koro.domain.interactor.SaveQuoteUseCase
 import kioli.koro.domain.model.QuoteModelDomain
+import kioli.koro.presentation.Events
 import kioli.koro.presentation.data.Resource
 import kioli.koro.presentation.data.ResourceState
+import kioli.koro.presentation.eventResult
 import kioli.koro.presentation.mapper.QuotePresentationMapper
 import kioli.koro.presentation.model.QuoteModelPresentation
 import javax.inject.Inject
@@ -35,6 +38,7 @@ open class IntroViewModel @Inject internal constructor(
     fun loadQuote() {
         quoteLiveData.postValue(Resource(ResourceState.LOADING, null, null))
         loadQuoteUseCase.execute(LoadQuoteSubscriber())
+        Analytics.trackEvent(Events.LoadQuoteStart.id)
     }
 
     inner class LoadQuoteSubscriber : DisposableSubscriber<QuoteModelDomain>() {
@@ -42,12 +46,14 @@ open class IntroViewModel @Inject internal constructor(
         override fun onComplete() {}
 
         override fun onNext(quote: QuoteModelDomain?) {
+            Analytics.trackEvent(Events.LoadQuoteResultOk.id, mutableMapOf(Pair(eventResult, quote.toString())))
             quote?.let {
                 quoteLiveData.postValue(Resource(ResourceState.SUCCESS, mapper.mapToPresentation(it), null))
             }
         }
 
         override fun onError(exception: Throwable) {
+            Analytics.trackEvent(Events.LoadQuoteResultFail.id, mutableMapOf(Pair(eventResult, exception.localizedMessage)))
             quoteLiveData.postValue(Resource(ResourceState.ERROR, null, exception.localizedMessage))
         }
     }
@@ -55,28 +61,35 @@ open class IntroViewModel @Inject internal constructor(
     fun saveQuote(text: String) {
         val quoteModel = QuoteModelPresentation(text)
         saveQuoteUseCase.execute(SaveQuoteSubscriber(), mapper.mapFromPresentation(quoteModel))
+        Analytics.trackEvent(Events.SaveQuoteStart.id)
     }
 
     inner class SaveQuoteSubscriber : DisposableCompletableObserver() {
 
-        override fun onComplete() {}
+        override fun onComplete() {
+            Analytics.trackEvent(Events.SaveQuoteResultOk.id)
+        }
 
         override fun onError(exception: Throwable) {
+            Analytics.trackEvent(Events.SaveQuoteResultFail.id, mutableMapOf(Pair(eventResult, exception.localizedMessage)))
             quoteLiveData.postValue(Resource(ResourceState.ERROR, null, exception.localizedMessage))
         }
     }
 
     fun clearQuotes() {
         clearQuoteUseCase.execute(ClearQuoteSubscriber(), "")
+        Analytics.trackEvent(Events.LoadQuoteStart.id)
     }
 
     inner class ClearQuoteSubscriber : DisposableCompletableObserver() {
 
         override fun onComplete() {
+            Analytics.trackEvent(Events.ClearQuoteResultOk.id)
             quoteLiveData.postValue(Resource(ResourceState.SUCCESS, null, null))
         }
 
         override fun onError(exception: Throwable) {
+            Analytics.trackEvent(Events.ClearQuoteResultFail.id, mutableMapOf(Pair(eventResult, exception.localizedMessage)))
             quoteLiveData.postValue(Resource(ResourceState.ERROR, null, exception.localizedMessage))
         }
     }
